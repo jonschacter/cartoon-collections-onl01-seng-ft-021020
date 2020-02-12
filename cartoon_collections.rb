f31e8466a799881894174e5ae17bd5d88f36447f
Jon Schacter <schacter.jon@gmail.com>
2020-02-12 19:21:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,3 +9,7 @@ def summon_captain_planet(planeteer_calls)
     element.capitalize << "!"
   end
 end
+
+def long_planeteer_calls(planeteer_calls)
+  long_planeteer_calls.detect { |call| call.length > 4 }
+end
